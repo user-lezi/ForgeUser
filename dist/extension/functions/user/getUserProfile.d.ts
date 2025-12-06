70c89a9916c1aec20c653ae348a26146b5139cd7
user-lezi <leleziar@gmail.com>
2025-12-06 17:13:57 +0530

    enhance $getUserProfile function to support additional user profile properties and separators

--- dist/extension/functions/user/getUserProfile.d.ts
+++ dist/extension/functions/user/getUserProfile.d.ts
@@ -1,3 +1,4 @@
 import { ArgType, NativeFunction } from "@tryforge/forgescript";
-declare const _default: NativeFunction<[import("@tryforge/forgescript").IArg<ArgType.String, true, false, import("@tryforge/forgescript").EnumLike>, import("@tryforge/forgescript").IArg<ArgType.Boolean, boolean, false, import("@tryforge/forgescript").EnumLike>], true>;
+import { UserProfileProperty } from "../../properties/userProfile";
+declare const _default: NativeFunction<[import("@tryforge/forgescript").IArg<ArgType.String, true, false, import("@tryforge/forgescript").EnumLike>, import("@tryforge/forgescript").IArg<ArgType.Boolean, boolean, false, import("@tryforge/forgescript").EnumLike>, import("@tryforge/forgescript").IArg<ArgType.Enum, boolean, false, typeof UserProfileProperty>, import("@tryforge/forgescript").IArg<ArgType.String, boolean, false, import("@tryforge/forgescript").EnumLike>], true>;
 export default _default;
--- src/extension/functions/user/getUserProfile.ts
+++ src/extension/functions/user/getUserProfile.ts
@@ -1,5 +1,9 @@
 import { Arg, ArgType, NativeFunction } from "@tryforge/forgescript";
 import { ForgeUser } from "../..";
+import {
+  UserProfileProperties,
+  UserProfileProperty,
+} from "../../properties/userProfile";
 
 export default new NativeFunction({
   name: "$getUserProfile",
@@ -10,12 +14,14 @@ export default new NativeFunction({
   args: [
     Arg.requiredString("id", "The user's id."),
     Arg.optionalBoolean("force", "Forcefully fetch the information."),
+    Arg.optionalEnum(UserProfileProperty, "property", "The property to pull."),
+    Arg.optionalString("separator", "Separator to use in case of array"),
   ],
 
   version: "1.0.0",
   output: ArgType.String,
 
-  async execute(ctx, [id, force]) {
+  async execute(ctx, [id, force, prop, sep]) {
     const rest = ctx.client.getExtension(ForgeUser, true).rest;
 
     try {
@@ -23,7 +29,8 @@ export default new NativeFunction({
         force: force == null ? false : force,
       });
 
-      return this.successJSON(data);
+      if (!prop) return this.successJSON(data);
+      return this.success(UserProfileProperties[prop](data, sep ?? ", "));
     } catch (err) {
       return this.customError(`REST Error: ${err}`);
     }
