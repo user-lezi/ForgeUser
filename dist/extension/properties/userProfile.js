79bd11a26fc346752b536ecc6a86a693d7c9f9e4
user-lezi <leleziar@gmail.com>
2025-12-07 12:37:09 +0530

    fix incomplete CDN

--- dist/extension/properties/userProfile.js
+++ dist/extension/properties/userProfile.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserProfileProperties = exports.UserProfileProperty = void 0;
 const defineProperties_1 = __importDefault(require("@tryforge/forgescript/dist/functions/defineProperties"));
 const discord_js_1 = require("discord.js");
+function cdn(s) {
+    return `https://cdn.discordapp.com${s}`;
+}
 var UserProfileProperty;
 (function (UserProfileProperty) {
     UserProfileProperty["id"] = "id";
@@ -26,22 +29,22 @@ var UserProfileProperty;
 })(UserProfileProperty || (exports.UserProfileProperty = UserProfileProperty = {}));
 exports.UserProfileProperties = (0, defineProperties_1.default)({
     id: (i) => i?.user.id,
-    avatar: (i) => i?.user.avatar
+    avatar: (i) => cdn(i?.user.avatar
         ? discord_js_1.CDNRoutes.userAvatar(i.user.id, i.user.avatar, i.user.avatar.startsWith("a_") ? "gif" : "png")
         :
-            discord_js_1.CDNRoutes.defaultUserAvatar((i.user.id >> 22) % 6),
+            discord_js_1.CDNRoutes.defaultUserAvatar((i.user.id >> 22) % 6)),
     badges: (i, sep) => i?.badges.map((x) => x.id).join(sep || ", "),
     globalName: (i) => i?.user.global_name,
     username: (i) => i?.user.username,
     banner: (i) => i?.user.banner
-        ? discord_js_1.CDNRoutes.userBanner(i.user.id, i.user.banner, i.user.banner.startsWith("a_") ? "gif" : "png")
+        ? cdn(discord_js_1.CDNRoutes.userBanner(i.user.id, i.user.banner, i.user.banner.startsWith("a_") ? "gif" : "png"))
         : "",
     accentColor: (i) => i?.user.accent_color,
     bannerColor: (i) => i?.user.banner_color,
     bio: (i) => i?.user.bio,
     clanID: (i) => i?.user.clan?.identity_guild_id,
     clanBadge: (i) => i?.user.clan?.badge
-        ? discord_js_1.CDNRoutes.guildTagBadge(i.user.clan.identity_guild_id, i.user.clan.badge, i.user.clan.badge.startsWith("a_") ? "gif" : "png")
+        ? cdn(discord_js_1.CDNRoutes.guildTagBadge(i.user.clan.identity_guild_id, i.user.clan.badge, i.user.clan.badge.startsWith("a_") ? "gif" : "png"))
         : "",
     clanTag: (i) => i?.user.clan?.tag,
     premiumType: (i) => discord_js_1.UserPremiumType[i?.premium_type],
--- src/extension/properties/userProfile.ts
+++ src/extension/properties/userProfile.ts
@@ -2,6 +2,10 @@ import defineProperties from "@tryforge/forgescript/dist/functions/definePropert
 import { CDNRoutes, UserPremiumType } from "discord.js";
 import { IUserProfile } from "../../core/types";
 
+function cdn<S extends string>(s: S) {
+  return `https://cdn.discordapp.com${s}`;
+}
+
 export enum UserProfileProperty {
   id = "id",
   username = "username",
@@ -26,27 +30,31 @@ export const UserProfileProperties = defineProperties<
 >({
   id: (i) => i?.user.id,
   avatar: (i) =>
-    i?.user.avatar
-      ? CDNRoutes.userAvatar(
-          i.user.id,
-          i.user.avatar,
+    cdn(
+      i?.user.avatar
+        ? CDNRoutes.userAvatar(
+            i.user.id,
+            i.user.avatar,
 
-          // @ts-ignore - im lzy
-          i.user.avatar.startsWith("a_") ? "gif" : "png",
-        )
-      : // @ts-ignore - im lzy
-        CDNRoutes.defaultUserAvatar((i!.user.id >> 22) % 6),
+            // @ts-ignore - im lzy
+            i.user.avatar.startsWith("a_") ? "gif" : "png",
+          )
+        : // @ts-ignore - im lzy
+          CDNRoutes.defaultUserAvatar((i!.user.id >> 22) % 6),
+    ),
   badges: (i, sep) => i?.badges.map((x) => x.id).join(sep || ", "),
   globalName: (i) => i?.user.global_name,
   username: (i) => i?.user.username,
   banner: (i) =>
     i?.user.banner
-      ? CDNRoutes.userBanner(
-          i.user.id,
-          i.user.banner,
+      ? cdn(
+          CDNRoutes.userBanner(
+            i.user.id,
+            i.user.banner,
 
-          // @ts-ignore - im lzy
-          i.user.banner.startsWith("a_") ? "gif" : "png",
+            // @ts-ignore - im lzy
+            i.user.banner.startsWith("a_") ? "gif" : "png",
+          ),
         )
       : "",
   accentColor: (i) => i?.user.accent_color,
@@ -55,11 +63,13 @@ export const UserProfileProperties = defineProperties<
   clanID: (i) => i?.user.clan?.identity_guild_id,
   clanBadge: (i) =>
     i?.user.clan?.badge
-      ? CDNRoutes.guildTagBadge(
-          i.user.clan.identity_guild_id!,
-          i.user.clan.badge,
-          // @ts-ignore - im lzy
-          i.user.clan.badge.startsWith("a_") ? "gif" : "png",
+      ? cdn(
+          CDNRoutes.guildTagBadge(
+            i.user.clan.identity_guild_id!,
+            i.user.clan.badge,
+            // @ts-ignore - im lzy
+            i.user.clan.badge.startsWith("a_") ? "gif" : "png",
+          ),
         )
       : "",
   clanTag: (i) => i?.user.clan?.tag,
